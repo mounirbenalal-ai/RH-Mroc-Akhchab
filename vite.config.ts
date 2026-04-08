7c7d588880b348c2cd9cc6cebcbd4c3466e95e77
Your Name <mounirbenalal@gmail.com>
2026-04-08 16:12:01 +0100

    System auto-update: Wed Apr  8 16:12:00 +01 2026

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// إعدادات النشر لـ GitHub Pages
 export default defineConfig({
   plugins: [react()],
   base: '/RH-Mroc-Akhchab/', 
